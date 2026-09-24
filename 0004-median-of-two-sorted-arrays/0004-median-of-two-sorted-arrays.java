78c8eb880dcf727df004a63e252016f1af336eb2
Aryann Gautam <sharmaaryan1516@gmail.com>
2026-09-24 19:52:57 +0530

    Time: 26 ms (13.28%), Space: 48.7 MB (85.86%) - LeetHub - Time: O((m+n)^2), Space: O(m+n)

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,32 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length + nums2.length];
+        int k = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            arr[k] = nums1[i];
+            k++;
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            arr[k] = nums2[i];
+            k++;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int n = arr.length;
+        if (n % 2 != 0) {
+            return arr[n / 2];
+        }
+        return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
