c7d5fe6912915c728db264e6e80e01a869ccd4b8
Aryann Gautam <sharmaaryan1516@gmail.com>
2026-09-24 20:44:06 +0530

    Time: 1 ms (99.96%), Space: 42.7 MB (26.84%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+        for (; x != 0; x = x / 10) {
+            int digit = x % 10;
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                reverse < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            reverse = reverse * 10 + digit;
+        }
+        return reverse;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
