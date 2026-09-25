416f5fd017f93c7b2c5cdb8c04741de43f458706
Aryann Gautam <sharmaaryan1516@gmail.com>
2026-09-25 14:52:30 +0530

    Time: 4 ms (31.63%), Space: 46.7 MB (8.92%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40,
+                        10, 9, 5, 4, 1};
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL",
+                          "X", "IX", "V", "IV", "I"};
+        String ans = "";
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                ans += roman[i];
+                num -= values[i];
+            }
+        }
+        return ans;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
